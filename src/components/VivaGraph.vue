fab55fcee9183893f68e8ef727ed27165741a1ef
44120554856 <jonathansilva@cge.mt.gov.br>
2024-08-02 13:37:06 -0400

    Primeira implementação

---
+++ src/components/VivaGraph.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="gh" class='graph relative-position fit'></div>
+</template>
+
+<script setup>
+import Viva from 'vivagraphjs'
+import { onMounted, ref } from 'vue'
+
+const gh = ref(null)
+
+var graphGenerator = Viva.Graph.generator();
+var graph = graphGenerator.grid(10, 10);
+
+function updateRenderer(graph) {
+  var graphics = Viva.Graph.View.webglGraphics()
+  var renderer = Viva.Graph.View.renderer(graph, {
+    graphics,
+    container: gh.value
+  });
+  renderer.run();
+}
+
+onMounted(() => {
+  updateRenderer(graph);
+})
+</script>
+
+<style lang="scss">
+.graph {
+  svg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+}
+</style>
+
+
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="page-full">
+    <viva-graph />
+  </q-page>
+</template>
+
+<script setup>
+import VivaGraph from 'components/VivaGraph.vue';
+
+defineOptions({
+  name: 'IndexPage'
+});
+
+</script>
+<style scoped>
+.page-full{
+  height: calc(100vh - 50px);
+}
+</style>
